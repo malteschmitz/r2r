caf95348a4bb9a94a8ab7482bf7834db76b7c68f
Malte Schmitz <schmitz@isp.uni-luebeck.de>
2021-02-21 16:44:06 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "prefer-const": ["error", {
+      "destructuring": "any",
+      "ignoreReadBeforeAssign": false
+    }]
+  }
+};
